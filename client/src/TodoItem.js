166c92df69a5ae3e61f58d2eb0b9b79ebd31c6a3
Parker <parkertleavitt@gmail.com>
2022-11-17 20:59:50 -0600

    create clearCompleted function in context

--- client/src/TodoItem.js
+++ client/src/TodoItem.js
@@ -1,24 +1,22 @@
 import { BsCheckCircleFill, BsCircle } from "react-icons/bs";
+import { useAppContext } from "./context/appContext";
 
 const TodoItem = ({ item }) => {
-  if (item.status === "completed") {
-    return (
-      <div className="bg-zinc-800 hover:bg-zinc-700 flex px-4 py-4 border text-white items-center m-3 mb-0 cursor-pointer">
-        <BsCircle className="text-xl" />
-        <p className="px-3 text-xl m-w-1/2 whitespace-nowrap truncate">
-          Create Portfolio asdfjd skafadjf lksajflksja dklf
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="bg-zinc-800 hover:bg-zinc-700 flex px-4 py-4 border text-white items-center m-3 last:mb-3 cursor-pointer">
+  const { toggleTodoStatus } = useAppContext();
+  return (
+    <div
+      className="bg-zinc-800 hover:bg-zinc-700 flex px-4 py-4 border text-white items-center m-3 mb-0 cursor-pointer"
+      onClick={() => toggleTodoStatus(item._id)}
+    >
+      {item.status === "completed" ? (
         <BsCheckCircleFill className="text-xl" />
-        <p className="px-3 text-xl m-w-1/2 whitespace-nowrap truncate line-through">
-          Create Portfolio asdfjd skafadjf lksajflksja dklf
-        </p>
-      </div>
-    );
-  }
+      ) : (
+        <BsCircle className="text-xl" />
+      )}
+      <p className={`px-3 text-xl m-w-1/2 whitespace-nowrap truncate ${item.status === "completed" ? "line-through" : ""}`}>
+        {item.todo}
+      </p>
+    </div>
+  );
 };
 export default TodoItem;
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -5,8 +5,9 @@ const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
   const [todoList, setTodoList] = useState([]);
-  const [totalItems, setTotalItems] = useState(0);
+  const [todoCount, setTodoCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
   const [alertText, setAlertText] = useState("");
   const [alertType, setAlertType] = useState("");
 
@@ -15,19 +16,68 @@ const AppProvider = ({ children }) => {
   const displayAlert = (message, type) => {
     setAlertText(message);
     setAlertType(type);
+    setShowAlert(true);
+
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
   };
 
   const getTodos = async () => {
     setIsLoading(true);
+    console.log("getting todos");
+
+    const url = baseURL + "/todos";
 
     try {
-      const { data } = axios.get(baseURL + "/todos");
+      const { data } = await axios.get(url);
       const { todoItems, totalItems } = data;
-      console.log(todoItems);
 
       setTodoList(todoItems);
-      setTotalItems(totalItems);
+      setTodoCount(totalItems);
       setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      displayAlert(error, "danger");
+    }
+  };
+
+  const createTodo = async (todo) => {
+    setIsLoading(true);
+
+    try {
+      await axios.post(baseURL + "/todos", { todo });
+      setIsLoading(false);
+      getTodos();
+      displayAlert("Todo created", "success");
+    } catch (error) {
+      setIsLoading(false);
+      displayAlert(error, "danger");
+    }
+  };
+
+  const toggleTodoStatus = async (id) => {
+    setIsLoading(true);
+
+    try {
+      await axios.patch(baseURL + `/todos/${id}`);
+      setIsLoading(false);
+      getTodos();
+      displayAlert("Todo updated!", "success");
+    } catch (error) {
+      setIsLoading(false);
+      displayAlert(error, "danger");
+    }
+  };
+
+  const clearCompleted = async () => {
+    setIsLoading(true);
+
+    try {
+      await axios.post(baseURL + "/todos/clear");
+      setIsLoading(false);
+      getTodos();
+      displayAlert("Completed todos have been cleared!", "success");
     } catch (error) {
       setIsLoading(false);
       displayAlert(error, "danger");
@@ -38,12 +88,16 @@ const AppProvider = ({ children }) => {
     <AppContext.Provider
       value={{
         todoList,
-        totalItems,
+        todoCount,
         isLoading,
         alertText,
         alertType,
         displayAlert,
         getTodos,
+        createTodo,
+        toggleTodoStatus,
+        clearCompleted,
+        showAlert,
       }}
     >
       {children}
