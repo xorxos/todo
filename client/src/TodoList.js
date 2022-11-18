0241a4e36b2657f0334cd2715e5ba741068fd5d6
Parker <parkertleavitt@gmail.com>
2022-11-17 23:06:45 -0600

    add no items default text

--- client/src/TodoList.js
+++ client/src/TodoList.js
@@ -7,6 +7,7 @@ const TodoList = () => {
 
   return (
     <div className="bg-zinc-800 min-w-full flex flex-col justify-start">
+      {todoList.length === 0 && <div className="text-white text-center my-3 mt-5 text-lg">No todo items found...</div>}
       {todoList.map((item, index) => (
         <TodoItem item={item} key={index} />
       ))}
